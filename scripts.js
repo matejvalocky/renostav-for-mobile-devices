259e1566297a012fdee09d5bf5aa44a41e235f18
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2022-12-27 23:46:04 +0100

    dokoncenie prvého dropdown menu

--- scripts.js
+++ scripts.js
@@ -15,5 +15,17 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+
+
+// vyskakovacie menu - dropdown menu //
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%"; 
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
